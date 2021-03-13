c320be2683e6db30e01d080207147a54bb252a80
simonkharel <47453356+simonkharel@users.noreply.github.com>
2021-03-13 10:49:43 -0600

    updated styling

--- components/admin-components/GenerateStudentSurveys.js
+++ components/admin-components/GenerateStudentSurveys.js
@@ -6,16 +6,20 @@ import {
   List,
   ListItem,
   Link,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
 } from "@chakra-ui/react";
 import { getCoursesByDepartment } from "../../api/APIHelper";
-import { listParser } from "@chakra-ui/styled-system";
-import useToggle from "../../hooks/useToggle"
+import useToggle from "../../hooks/useToggle";
 const GenerateStudentSurveys = () => {
-
-
-
   const [courseList, setCourses] = useState({ courses: [] });
-  const [isOn,toggleOn]= useToggle()
+  const [isOn, toggleOn] = useToggle();
   const getNewCourses = async () => {
     const newCourseList = await getCoursesByDepartment("CSCE");
     setCourses({
@@ -24,88 +28,85 @@ const GenerateStudentSurveys = () => {
     });
     console.log(courseList.courses);
   };
-  const setButtonHandler =()=>{
-    setButtonName("Sort by Course Number")
-    
-  }
+  const setButtonHandler = () => {
+    setButtonName("Sort by Course Number");
+  };
   const surveyCourses = []
     .concat(courseList.courses)
     .sort((a, b) => (a.courseNumber > b.courseNumber ? 1 : -1))
     .map((course, idx) => {
       return (
         <>
-        <ListItem key={idx}  bg={idx % 2 == 0 ? "gray.300" : "gray.100"}>
-          {course.department} {course.courseNumber}.{course.sectionNumber}{" "}
-          {course.displayName}{" "}
-          <br></br>
-          <Text>
-            {course.coordinator.firstName} {course.coordinator.lastName}
-          </Text>
-          <Link color="teal" href="https://www.unt.edu/">Student Survey Link</Link>
-        </ListItem>
+          <Tr key={idx} bg={idx % 2 == 0 ? "gray.300" : "gray.100"}>
+            <Td>
+              {course.department} {course.courseNumber},{course.displayName},
+              Intsructor: {course.coordinator.firstName}{" "}
+              {course.coordinator.lastName}, Survey Link:{" "}
+              <Link color="teal" href="https://www.unt.edu/">
+                Student Survey Link
+              </Link>
+            </Td>
+            <br></br>
+          </Tr>
         </>
       );
     });
 
-    const surveyCoursesByInstuctor = []
+  const surveyCoursesByInstuctor = []
     .concat(courseList.courses)
     .sort((a, b) => (a.lastName > b.lastName ? 1 : -1))
     .map((course, idx) => {
-      console.log(course)
+      console.log(course);
       return (
         <>
-        <ListItem key={idx}  bg={idx % 2 == 0 ? "gray.300" : "gray.100"}>
-          
-          {course.department} {course.courseNumber}.{course.sectionNumber}{" "}
-          {course.displayName} {" "}
-          <br></br>
-          <Text>
-            {course.coordinator.firstName} {course.coordinator.lastName}
-          </Text>
-          <Link color="teal" href="https://www.unt.edu/">Student Survey Link</Link>
-        </ListItem>
+          <Tr key={idx} bg={idx % 2 == 0 ? "gray.300" : "gray.100"}>
+            <Td>
+              {course.department} {course.courseNumber},{course.displayName},
+              Intsructor: {course.coordinator.firstName}{" "}
+              {course.coordinator.lastName}, Survey Link:{" "}
+              <Link color="teal" href="https://www.unt.edu/">
+                Student Survey Link
+              </Link>
+            </Td>
+            <br></br>
+          </Tr>
         </>
       );
     });
   useEffect(() => {
     getNewCourses();
   }, []);
-  const show = isOn
+  const show = isOn;
   if (show) {
-    return(
+    return (
       <div id="top">
-      <Box borderWidth="2px" rounded="lg" alignItems="center" m="1em" p="3em">
-        <div>
-          <Box>
-            <Text fontWeight="bold" fontSize="lg" mb="1em">
-              Generate Student Surveys
-            </Text>
-            <Button onClick={toggleOn} mb="1em">Sort by Course Number</Button>
-            <List>{surveyCoursesByInstuctor}</List>
-            
-          </Box>
-        </div>
-      </Box>
-    </div>
-    )
+        <Box borderWidth="2px" rounded="lg" alignItems="center" m="1em" p="3em">
+        <Table variant="striped" colorScheme="green" padding="1em">
+        <TableCaption placement="top" fontWeight="bold" fontSize="xl">GenerateStudentSurveys</TableCaption>
+              <Button onClick={toggleOn} mb="1em">
+                Sort by Course Number
+              </Button>
+              <Tbody>{surveyCoursesByInstuctor}</Tbody>
+          </Table>
+        </Box>
+      </div>
+    );
   }
-  if(!show){
-    return(
+  if (!show) {
+    return (
       <div id="top">
-      <Box borderWidth="2px" rounded="lg" alignItems="center" m="1em" p="3em">
-        <div>
-          <Box>
-            <Text fontWeight="bold" fontSize="lg" mb="1em">
-              Generate Student Surveys
-            </Text>
-            <Button onClick={toggleOn} mb="1em">Sort by Instructors</Button>
-            <List>{surveyCourses}</List>
-          </Box>
-        </div>
-      </Box>
-    </div>
-    )
+        <Box borderWidth="2px" rounded="lg" alignItems="center" m="1em" p="3em">
+        <Table variant="striped" colorScheme="green" padding="1em">
+        <TableCaption placement="top" fontWeight="bold" fontSize="xl">GenerateStudentSurveys</TableCaption>
+              <Button onClick={toggleOn} mb="1em">
+                Sort by Course Number
+              </Button>
+              <Tbody>{surveyCourses}</Tbody>
+          </Table>
+        </Box>
+      </div>
+    );
   }
-
 };
 export default GenerateStudentSurveys;
+
