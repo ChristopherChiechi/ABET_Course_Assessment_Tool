178399a337af274d9d021d038cb3ef8704ff6994
rmschick99 <schick.ryan@yahoo.com>
2021-03-20 03:50:26 -0500

    Uncommented paths to added components

--- pages/adminHome.js
+++ pages/adminHome.js
@@ -1,18 +1,17 @@
-import{ useState } from 'react';
-import Head from 'next/head'
-// import AdminNavigation from '../components/admin-components/AdminNavigation/AdminNavigation';
-import AdminMenu from '../components/admin-components/AdminMenu';
+import { useState } from "react";
+import Head from "next/head";
+import AdminNavigation from "../components/admin-components/AdminNavigation";
+import AdminMenu from "../components/admin-components/AdminMenu";
 // import FullReport from '../components/admin-components/FullReport/FullReport';
-// import CreateNewSemester from '../components/admin-components/CreateNewSemester/CreateNewSemester';
+import CreateNewSemester from "../components/admin-components/CreateNewSemester";
 // import EditProgramList from '../components/admin-components/EditProgramList/EditProgramList';
-// import EditCourseAssignments from '../components/admin-components/EditCourseAssignments/EditCourseAssignments';
-// import EditFacultyList from '../components/admin-components/EditFacultyList/EditFacultyList';
- import EditCourseList from '../components/admin-components/EditCourseList/EditCourseList';
+import EditCourseAssignments from "../components/admin-components/EditCourseAssignments/EditCourseAssignments";
+import EditFacultyList from "../components/admin-components/EditFacultyList/EditFacultyList";
+import EditCourseList from "../components/admin-components/EditCourseList/EditCourseList";
 // import EditCourseOutcomes from '../components/admin-components/EditCourseOutcomes/EditCourseOutcomes';
 import OutcomeMapping from "../components/admin-components/OutcomeMapping/OutcomeMapping";
 // import GenerateSimpleReport from '../components/admin-components/GenerateSimpleReport/GenerateSimpleReport';
 import GenerateStudentSurveys from "../components/admin-components/GenerateStudentSurveys";
-
 
 const adminHome = () => {
   const [user, setUser] = useState("MT2020");
@@ -22,32 +21,33 @@ const adminHome = () => {
       <Head>
         <title>UNT ABET: Admin Page</title>
       </Head>
-      <div >
+      <AdminNavigation user={user} />
+      <div>
         <div>
           <AdminMenu setView={setView} />
         </div>
         <div>
           {(() => {
             switch (view) {
-            // case "GFR":
-            //     return <FullReport />;
-            //   case "GSR":
-            //     return <GenerateSimpleReport user={user}/>;
+              // case "GFR":
+              //     return <FullReport />;
+              //   case "GSR":
+              //     return <GenerateSimpleReport user={user}/>;
               case "OM":
                 return <OutcomeMapping />;
-            //   case "CNS":
-            //     return <CreateNewSemester />;
-            //   case "EAC":
-            //     return <EditCourseAssignments />;
-            //   case "EFL":
-            //     return <EditFacultyList />;
-            //   case "EPL":
-            //     return <EditProgramList />;
-               case "ECL":
-                 return <EditCourseList/>;
-            //   case "ECO":
-            //     return <EditCourseOutcomes/>;
-            case "GSS":
+              case "CNS":
+                return <CreateNewSemester />;
+              case "EAC":
+                return <EditCourseAssignments />;
+              case "EFL":
+                return <EditFacultyList />;
+              //   case "EPL":
+              //     return <EditProgramList />;
+              case "ECL":
+                return <EditCourseList />;
+              //   case "ECO":
+              //     return <EditCourseOutcomes/>;
+              case "GSS":
                 return <GenerateStudentSurveys />;
             }
           })()}
