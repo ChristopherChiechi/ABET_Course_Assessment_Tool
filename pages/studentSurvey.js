73e70871b9d5548a9622e5221209cb9e38ff82a8
simonkharel <47453356+simonkharel@users.noreply.github.com>
2021-03-23 11:44:35 -0500

    added state to student information and text areas

--- pages/studentSurvey.js
+++ pages/studentSurvey.js
@@ -28,18 +28,26 @@ const studentSurvey = () => {
   const grades = ["A", "B", "C", "D", "F"];
   const course = courseData.courses[0];
   const questions = questionData.TAquestions;
-  console.log(course);
-  console.log(questions);
-  const [studentInformation, setStudentInformation] = useState({
-    major: "",
-    year: "",
-    anticipatedGrade: "",
+  const [studentInformationMajor, setStudentInformationMajor] = useState({
+    major: "major",
   });
-  const studentInformationHandler = (event) => {
-    setStudentInformation({
-      major: event.target.value,
-    });
-    console.log(major);
+  const [studentInformationYear, setStudentInformationYear] = useState({
+    year: "year",
+  });
+  const [studentInformationGrades, setStudentInformationGrades] = useState({
+    grades: "grades",
+  });
+  const [taPerformanceText, setTaPerformanceText] = useState({ text: "" });
+  const [classPerformanceText, setClassPerformanceText] = useState({
+    text: "",
+  });
+
+  const testFunction = () => {
+    console.log(studentInformationMajor.major);
+    console.log(studentInformationYear.year);
+    console.log(studentInformationGrades.grades);
+    console.log(taPerformanceText.text);
+    console.log(classPerformanceText.text);
   };
   return (
     <VStack mt="2em">
@@ -50,42 +58,48 @@ const studentSurvey = () => {
 
       <HStack w="80%">
         <Text>What is your major?</Text>
-        <Select placeholder="major">
+        <Select
+          placeholder="major"
+          value={studentInformationMajor.major}
+          onChange={(e) =>
+            setStudentInformationMajor({ major: e.target.value })
+          }
+        >
           {majors.map((major, idx) => {
             return (
-              <option key={idx} onChange={studentInformationHandler}>
+              <option value={major} key={idx}>
                 {major}
               </option>
             );
           })}
         </Select>
       </HStack>
-
       <HStack w="80%">
         <Text>What is your classification?</Text>
-        <Select placeholder="classifcation">
+        <Select
+          placeholder="classifcation"
+          value={studentInformationYear.year}
+          onChange={(e) => setStudentInformationYear({ year: e.target.value })}
+        >
           {years.map((year, idx) => {
-            return (
-              <option key={idx} onChange={studentInformationHandler}>
-                {year}{" "}
-              </option>
-            );
+            return <option key={idx}>{year} </option>;
           })}
         </Select>
       </HStack>
       <HStack w="80%">
         <Text>What grade do you expect?</Text>
-        <Select placeholder="grade">
+        <Select
+          placeholder="grade"
+          value={studentInformationGrades.grades}
+          onChange={(e) =>
+            setStudentInformationGrades({ grades: e.target.value })
+          }
+        >
           {grades.map((year, idx) => {
-            return (
-              <option key={idx} onChange={studentInformationHandler}>
-                {year}{" "}
-              </option>
-            );
+            return <option key={idx}>{year} </option>;
           })}
         </Select>
       </HStack>
-
       <Text fontWeight="bold">
         For each of the course outcomes, rate each from 1 to 5 in terms of
         agreement with your level of agreement.{" "}
@@ -156,7 +170,10 @@ const studentSurvey = () => {
         boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
       >
         <Text fontWeight="bold">Additional comments on TA's performance</Text>
-        <Textarea bg="white"></Textarea>
+        <Textarea
+          onChange={(e) => setTaPerformanceText({ text: e.target.value })}
+          bg="white"
+        ></Textarea>
       </Container>
 
       <Container
@@ -167,10 +184,13 @@ const studentSurvey = () => {
         boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
       >
         <Text fontWeight="bold">List ways this course can be improved</Text>
-        <Textarea bg="white"></Textarea>
+        <Textarea
+          onChange={(e) => setClassPerformanceText({ text: e.target.value })}
+          bg="white"
+        ></Textarea>
       </Container>
 
-      <Button>Submit Survey</Button>
+      <Button onClick={testFunction}>Submit Survey</Button>
     </VStack>
   );
 };
