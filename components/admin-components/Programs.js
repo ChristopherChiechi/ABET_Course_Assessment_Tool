e53a2f6a672395cd58222361ecb893065ea7ff81
rmschick99 <schick.ryan@yahoo.com>
2021-03-30 15:18:50 -0500

    Matched style of edit program list and changed array it is reading from

---
+++ components/admin-components/Programs.js
@@ -0,0 +1,23 @@
+import { Grid, GridItem, Text } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import useToggle from "../../hooks/useToggle";
+import { removeCourse } from "../../api/APIHelper";
+
+const Programs = ({
+  program,
+  color,
+}) => {
+  return (
+    <Grid templateColumns="repeat(1, 1fr)" bg={color} padding=".5em">
+      <>
+        <GridItem colSpan={1}>
+          <Text align="center" fontSize="20px">
+            {program}
+          </Text>
+        </GridItem>
+      </>
+    </Grid>
+  );
+};
+
+export default Programs;
