69988215715feb02f4bd22311e44b29ba6dc0c61
unknown <kharel_simon@yahoo.com>
2021-04-03 09:20:50 -0500

    validation checking for student information

--- pages/studentSurvey.js
+++ pages/studentSurvey.js
@@ -1,26 +1,20 @@
 //imports
-import {
-  useState,
-  useContext,
-  useEffect
-} from "react";
-import Head from 'next/head';
-import {
-  Text,
-  VStack,
-  Button,
-  Box
-} from "@chakra-ui/react";
+import { useState, useContext, useEffect } from "react";
+import Head from "next/head";
+import { Text, VStack, Button, Box } from "@chakra-ui/react";
 //hooks
-import useToggle from "../hooks/useToggle"
+import useToggle from "../hooks/useToggle";
 //components
 import StudentLoginBox from "../components/survey-components/StudentLoginBox";
 import StudentInfoForm from "../components/survey-components/StudentInfoForm";
 import CourseOutcomesSurvey from "../components/survey-components/CourseOutcomesSurvey";
 import TAsurvey from "../components/survey-components/TAsurvey";
 import StudentFeedback from "../components/survey-components/StudentFeedback";
-//page data 
-import { PageContext, pageData } from '../components/survey-components/SurveyPageData';
+//page data
+import {
+  PageContext,
+  pageData,
+} from "../components/survey-components/SurveyPageData";
 
 const studentSurvey = () => {
   const context = useContext(PageContext);
@@ -31,21 +25,30 @@ const studentSurvey = () => {
   const [studentInformation, setStudentInformation] = useState({
     major: "",
     classification: "",
-    grade: ""
+    grade: "",
   });
-  const [outcomeSurvey, setOutcomeSurvey] = useState(context.course['course-outcomes']);
-  const [TAquestions, setTAquestions] = useState(context.questions)
+  const [outcomeSurvey, setOutcomeSurvey] = useState(
+    context.course["course-outcomes"]
+  );
+  const [TAquestions, setTAquestions] = useState(context.questions);
   const [studentInput, setStudentInput] = useState({
     TAeval: "",
-    additionalFeedback: ""
-  })
+    additionalFeedback: "",
+  });
 
   useEffect(() => {
-    console.log('ss updated');
+    console.log("ss updated");
   }, [TAquestions]);
 
   const testFunction = () => {
     console.log(studentInformation, outcomeSurvey, TAquestions, studentInput);
+    if (
+      studentInformation.major === "" ||
+      studentInformation.classification == "" ||
+      studentInformation.grade === ""
+    ) {
+      alert("Fill out the student information");
+    }
   };
 
   const handleChange = (rating, idx, type) => {
@@ -54,14 +57,12 @@ const studentSurvey = () => {
       temp[idx].rating = parseInt(rating);
       setTAquestions([...temp]);
       console.log(TAquestions[idx]);
-    }
-    else if (type == "Outcomes") {
+    } else if (type == "Outcomes") {
       var temp = outcomeSurvey;
       outcomeSurvey[idx].rating = parseInt(rating);
       setOutcomeSurvey([...temp]);
       console.log(outcomeSurvey[idx]);
-    }
-    else {
+    } else {
       console.log("incorrect type");
     }
   };
@@ -71,24 +72,38 @@ const studentSurvey = () => {
       <Head>
         <title>UNT Course Feedback Survey</title>
       </Head>
-      {isLoggedIn
-        ?
+      {isLoggedIn ? (
         <VStack mt="2em">
-
           <Box w="80%">
-            <Text fontSize="2xl" fontWeight="bold">{course.code} {course.name}</Text>
-            <Text fontSize="xl" fontWeight="bold" color="green">Student Feedback Survey</Text>
+            <Text fontSize="2xl" fontWeight="bold">
+              {course.code} {course.name}
+            </Text>
+            <Text fontSize="xl" fontWeight="bold" color="green">
+              Student Feedback Survey
+            </Text>
           </Box>
-          <StudentInfoForm studentInformation={studentInformation} setStudentInformation={setStudentInformation} />
-          <CourseOutcomesSurvey outcomeSurvey={outcomeSurvey} handleChange={handleChange} />
+          <StudentInfoForm
+            studentInformation={studentInformation}
+            setStudentInformation={setStudentInformation}
+          />
+          <CourseOutcomesSurvey
+            outcomeSurvey={outcomeSurvey}
+            handleChange={handleChange}
+          />
           <TAsurvey TAquestions={TAquestions} handleChange={handleChange} />
-          <StudentFeedback studentInput={studentInput} setStudentInput={setStudentInput} />
+          <StudentFeedback
+            studentInput={studentInput}
+            setStudentInput={setStudentInput}
+          />
 
-          <Button onClick={testFunction} mb="1em" colorScheme="green">Submit Survey</Button>
+          <Button onClick={testFunction} mb="1em" colorScheme="green">
+            Submit Survey
+          </Button>
         </VStack>
-        :
+      ) : (
         <StudentLoginBox toggleLoggin={toggleLogin} />
-      }
-    </PageContext.Provider>)
+      )}
+    </PageContext.Provider>
+  );
 };
 export default studentSurvey;
