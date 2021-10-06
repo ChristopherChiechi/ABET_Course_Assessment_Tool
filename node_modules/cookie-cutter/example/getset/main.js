583b8e25875bf14e12155a9e0c5d861daf3e47e9
Jayden To <jaydento@my.unt.edu>
2021-10-06 17:09:49 -0500

    .gitignore is now working

--- node_modules/cookie-cutter/example/getset/main.js
+++
@@ -1,7 +0,0 @@
-var cookie = require('cookie-cutter');
-var times = parseInt(cookie.get('times'), 10) || 0;
-cookie.set('times', times + 1);
-
-window.onload = function () {
-    document.body.innerHTML += times + ' times';
-};
