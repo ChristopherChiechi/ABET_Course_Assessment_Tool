2dd7db0f3b041c1712ff9156aa7b51200c192a55
Chiechi, Christopher <christopherchiechi@my.unt.edu>
2021-10-19 16:39:40 -0500

    Updated header

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,27 @@
-import Image from 'next/image'
-//import { Image } from "@chakra-ui/react"
+//import Image from 'next/image'
+import { Image } from "@chakra-ui/react"
 import { Text, Box, HStack } from '@chakra-ui/react';
 
 const Header = () => {
     return (
-        <Box boxsize="sm" w="100%" bg="#016a31" height="15%" padding="1em">
-            {/*<Image 
-                src = {"/UNTbanner.png"} 
-                alt={"UNTbanner"}
-                //layout={"fill"}
-                objectFit={"none"}
-                quality={100}
-                width={211}
-                height={239}
-                priority
+        <>
+        <Box boxsize="sm" w="100%" bg="#016a31" height="15%" padding="0.75em">
+            <Image
+                src="/UNTbanner.png"
+                pos="absolute"
+                top="0"
+                left="3"
+                boxSize="96px"
             />
-            */}
             <HStack ml="7.5%">
-                <Text fontSize="4xl" fontWeight="bold" color="white">UNT</Text>
-                <Text fontSize="lg" fontWeight="bold" color="white">ABET Tool</Text>
+                <Text fontSize="sm" fontWeight="hairline" /*letterSpacing="tighter"*/ color="white">College of Engineering</Text>
+            </HStack>
+            <HStack ml="7.5%">
+                <Text fontSize="xl" fontWeight="bold" color="white">ABET Tool</Text>
             </HStack>
         </Box>
+        <Box boxsize="sm" w="100%" bg="black" height="5%" padding="0.3em"></Box>
+        </>
     )
 }
 export default Header;
