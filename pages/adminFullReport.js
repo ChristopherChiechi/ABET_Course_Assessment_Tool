9a035be03605b9d014022f9cb69c194cf1fec9cd
Jayden To <jaydento@my.unt.edu>
2022-04-12 14:34:20 -0500

    Update adminFullReport.js
    - Api is fully implement
    - Clean up the make pdf method

--- pages/adminFullReport.js
+++ pages/adminFullReport.js
@@ -1,15 +1,12 @@
 //imports
 import { useEffect, useState } from "react";
-import { Grid, Button, BreadcrumbLink } from "@chakra-ui/react";
+import { Grid, Button } from "@chakra-ui/react";
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
-import reportCS from "../fake-data/reportCS.json";
-import reportCE from "../fake-data/reportCE.json";
-import reportCYS from "../fake-data/reportCYS.json";
 import { GenerateFullReport } from "../api/APIHelper";
 import AdminReportTable from "../components/admin-components/AdminReport/AdminReportTable";
-
+import AdminNavigation from "../components/admin-components/AdminNavigation";
 // will need to update when backend is completed
 const adminFullReport = ({
   /*semester, year*/ number,
@@ -24,203 +21,6 @@ const adminFullReport = ({
       {
         text: "Spring 2023 Full Report",
         style: "header",
-      },
-      {
-        style: "tableExample",
-        color: "#111",
-        alignment: "center",
-        table: {
-          widths: [50, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35],
-          headerRows: 1,
-          // keepWithHeaderRows: 1,
-          body: [
-            [
-              {},
-              {
-                text: "Student Outcomes",
-                alignment: "center",
-                colSpan: 10,
-                style: "outcomeHeader",
-              },
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-            ],
-            [
-              {
-                text: "Course",
-                style: "outcomeHeader",
-                colSpan: 1,
-                alignment: "center",
-              },
-              { text: "1", style: "outcomeHeader" },
-              { text: "2", style: "outcomeHeader" },
-              { text: "3", style: "outcomeHeader" },
-              { text: "4", style: "outcomeHeader" },
-              { text: "5", style: "outcomeHeader" },
-              { text: "6", style: "outcomeHeader" },
-              { text: "7", style: "outcomeHeader" },
-              { text: "8", style: "outcomeHeader" },
-              { text: "9", style: "outcomeHeader" },
-              { text: "10", style: "outcomeHeader" },
-            ],
-          ],
-        },
-      },
-      {
-        style: "tableExample",
-        color: "#111",
-        alignment: "center",
-        pageBreak: "before",
-        table: {
-          widths: [50, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35],
-          headerRows: 1,
-          // keepWithHeaderRows: 1,
-          body: [
-            [
-              {},
-              {
-                text: "Student Outcomes",
-                alignment: "center",
-                colSpan: 10,
-                style: "outcomeHeader",
-              },
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-            ],
-            [
-              {
-                text: "Course",
-                style: "outcomeHeader",
-                colSpan: 1,
-                alignment: "center",
-              },
-              { text: "1", style: "outcomeHeader" },
-              { text: "2", style: "outcomeHeader" },
-              { text: "3", style: "outcomeHeader" },
-              { text: "4", style: "outcomeHeader" },
-              { text: "5", style: "outcomeHeader" },
-              { text: "6", style: "outcomeHeader" },
-              { text: "7", style: "outcomeHeader" },
-              { text: "8", style: "outcomeHeader" },
-              { text: "9", style: "outcomeHeader" },
-              { text: "10", style: "outcomeHeader" },
-            ],
-          ],
-        },
-      },
-      {
-        style: "tableExample",
-        color: "#111",
-        pageBreak: "before",
-
-        alignment: "center",
-        table: {
-          widths: [50, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35],
-          headerRows: 1,
-          // keepWithHeaderRows: 1,
-          body: [
-            [
-              {},
-              {
-                text: "Student Outcomes",
-                alignment: "center",
-                colSpan: 10,
-                style: "outcomeHeader",
-              },
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-            ],
-            [
-              {
-                text: "Course",
-                style: "outcomeHeader",
-                colSpan: 1,
-                alignment: "center",
-              },
-              { text: "1", style: "outcomeHeader" },
-              { text: "2", style: "outcomeHeader" },
-              { text: "3", style: "outcomeHeader" },
-              { text: "4", style: "outcomeHeader" },
-              { text: "5", style: "outcomeHeader" },
-              { text: "6", style: "outcomeHeader" },
-              { text: "7", style: "outcomeHeader" },
-              { text: "8", style: "outcomeHeader" },
-              { text: "9", style: "outcomeHeader" },
-              { text: "10", style: "outcomeHeader" },
-            ],
-          ],
-        },
-      },
-      {
-        style: "tableExample",
-        color: "#111",
-        pageBreak: "before",
-
-        alignment: "center",
-        table: {
-          widths: [50, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35],
-          headerRows: 1,
-          // keepWithHeaderRows: 1,
-          body: [
-            [
-              {},
-              {
-                text: "Student Outcomes",
-                alignment: "center",
-                colSpan: 10,
-                style: "outcomeHeader",
-              },
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-              {},
-            ],
-            [
-              {
-                text: "Course",
-                style: "outcomeHeader",
-                colSpan: 1,
-                alignment: "center",
-              },
-              { text: "1", style: "outcomeHeader" },
-              { text: "2", style: "outcomeHeader" },
-              { text: "3", style: "outcomeHeader" },
-              { text: "4", style: "outcomeHeader" },
-              { text: "5", style: "outcomeHeader" },
-              { text: "6", style: "outcomeHeader" },
-              { text: "7", style: "outcomeHeader" },
-              { text: "8", style: "outcomeHeader" },
-              { text: "9", style: "outcomeHeader" },
-              { text: "10", style: "outcomeHeader" },
-            ],
-          ],
-        },
       },
     ],
     styles: {
@@ -244,76 +44,91 @@ const adminFullReport = ({
   const [reportCSJson, setReportCSJson] = useState();
   const [reportCYSJson, setReportCYSJson] = useState();
 
-  const getITReport = async () => {
+  const getReportData = async () => {
     try {
-      const reportResponse = await GenerateFullReport(2023, "Spring", "IT");
+      const reportResponse = await GenerateFullReport(year, semester);
       const reportData = reportResponse.data;
-      setReportITJson(reportData);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const getCEReport = async () => {
-    try {
-      const reportResponse = await GenerateFullReport(2023, "Spring", "CE");
-      const reportData = reportResponse.data;
-      setReportCEJson(reportData);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const getCSReport = async () => {
-    try {
-      const reportResponse = await GenerateFullReport(2023, "Spring", "CE");
-      const reportData = reportResponse.data;
-      setReportCSJson(reportData);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const getCYSReport = async () => {
-    try {
-      const reportResponse = await GenerateFullReport(2023, "Spring", "CE");
-      const reportData = reportResponse.data;
-      setReportCYSJson(reportData);
+      if (reportData.CS) {
+        setReportCSJson(reportData.CS);
+      }
+      if (reportData.IT) {
+        setReportITJson(reportData.IT);
+      }
+      if (reportData.CE) {
+        setReportCEJson(reportData.CE);
+      }
+      if (reportData.CYS) {
+        setReportCYSJson(reportData.CYS);
+      }
+      console.log(reportData);
     } catch (err) {
       console.log(err);
     }
   };
 
   useEffect(() => {
-    getITReport();
-    getCEReport();
-    getCSReport();
-    getCYSReport();
+    getReportData();
   }, []);
 
   //Add
   const addMajorGradeToTable = (majorName, majorData) => {
-    var index = 0;
-    switch (majorName) {
-      case "Computer Science":
-        index = 1;
-        break;
-      case "Computer Engineering":
-        index = 2;
-        break;
-      case "Information Technology":
-        index = 3;
-        break;
-      case "Cyber Security":
-        index = 4;
-        break;
+    console.log(document.content.length);
+    var pagebreak = "none";
+    if (document.content.length > 1) {
+      pagebreak = "before";
     }
-    if (index == 0) {
-      console.log("Could not find major!");
-      return;
-    }
-    console.log(majorName, index);
-    console.log(document.content);
+    let obj = {
+      style: "tableExample1",
+      color: "#111",
+      alignment: "center",
+      pageBreak: pagebreak,
+      table: {
+        widths: [50, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35],
+        headerRows: 1,
+        // keepWithHeaderRows: 1,
+        body: [
+          [
+            {},
+            {
+              text: "Student Outcomes",
+              alignment: "center",
+              colSpan: 10,
+              style: "outcomeHeader",
+            },
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+          ],
+          [
+            {
+              text: "Course",
+              style: "outcomeHeader",
+              colSpan: 1,
+              alignment: "center",
+            },
+            { text: "1", style: "outcomeHeader" },
+            { text: "2", style: "outcomeHeader" },
+            { text: "3", style: "outcomeHeader" },
+            { text: "4", style: "outcomeHeader" },
+            { text: "5", style: "outcomeHeader" },
+            { text: "6", style: "outcomeHeader" },
+            { text: "7", style: "outcomeHeader" },
+            { text: "8", style: "outcomeHeader" },
+            { text: "9", style: "outcomeHeader" },
+            { text: "10", style: "outcomeHeader" },
+          ],
+        ],
+      },
+    };
+    var index = document.content.length;
+    document.content.push(obj);
+    console.log(document);
     document.content[index].table.body.unshift([
       {
         text: majorName,
@@ -354,54 +169,65 @@ const adminFullReport = ({
         { text: ArrOfOutcomes[9] },
       ]);
     }
-    console.log(document);
   };
 
   const createPdfFile = () => {
-    addMajorGradeToTable("Computer Science", reportCSJson);
-    addMajorGradeToTable("Computer Engineering", reportCEJson);
-    addMajorGradeToTable("Information Technology", reportITJson);
-    addMajorGradeToTable("Cyber Security", reportCYSJson);
+    if (reportCSJson) {
+      addMajorGradeToTable("Computer Science", reportCSJson);
+    }
+    if (reportCEJson) {
+      addMajorGradeToTable("Computer Engineering", reportCEJson);
+    }
+    if (reportITJson) {
+      addMajorGradeToTable("Information Technology", reportITJson);
+    }
+    if (reportCYSJson) {
+      addMajorGradeToTable("Cyber Security", reportCYSJson);
+    }
     pdfMake.createPdf(document).open();
     setTimeout(
       function () {
-        //window.location.reload();
+        window.location.reload();
       }.bind(this),
       1000
     );
   };
 
   return (
-    <Grid templateColumns="repeat(1, 1fr)" gap={8} margin="auto" w="80%">
-      <Button onClick={createPdfFile} colorScheme="teal" w="30%" mt="1%">
-        Download pdf
-      </Button>
-      {reportITJson && (
-        <AdminReportTable
-          id="MyTable"
-          reportITJson={reportITJson}
-          majorName={"Information Technology"}
-        />
-      )}
-      {reportCSJson && (
-        <AdminReportTable
-          reportITJson={reportCSJson}
-          majorName={"Computer Science"}
-        />
-      )}
-      {reportCYSJson && (
-        <AdminReportTable
-          reportITJson={reportCYSJson}
-          majorName={"Cyber Security"}
-        />
-      )}
-      {reportCEJson && (
-        <AdminReportTable
-          reportITJson={reportCEJson}
-          majorName={"Computer Engineering"}
-        />
-      )}
-    </Grid>
+    <>
+      <AdminNavigation />
+      <Grid templateColumns="repeat(1, 1fr)" gap={8} margin="auto" w="80%">
+        <Button onClick={createPdfFile} colorScheme="teal" w="30%" mt="1%">
+          Download pdf
+        </Button>
+        {reportITJson && (
+          <AdminReportTable
+            id="MyTable"
+            reportITJson={reportITJson}
+            majorName={"Information Technology"}
+          />
+        )}
+        {reportCSJson && (
+          <AdminReportTable
+            reportITJson={reportCSJson}
+            majorName={"Computer Science"}
+          />
+        )}
+        {reportCYSJson && (
+          <AdminReportTable
+            reportITJson={reportCYSJson}
+            majorName={"Cyber Security"}
+          />
+        )}
+        {reportCEJson && (
+          <AdminReportTable
+            reportITJson={reportCEJson}
+            majorName={"Computer Engineering"}
+          />
+        )}
+      </Grid>
+      );
+    </>
   );
 };
 
