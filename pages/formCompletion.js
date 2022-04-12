32c96dead24bda54987a0c045a797f765fd651ee
Jayden To <jaydento@my.unt.edu>
2022-04-12 14:38:12 -0500

    Update formCompletion.js
    Fix form completion to use use real

--- pages/formCompletion.js
+++ pages/formCompletion.js
@@ -5,7 +5,6 @@ import cookieCutter from "cookie-cutter";
 import jwt from "jsonwebtoken";
 
 import {
-  VStack,
   Button,
   Text,
   Box,
@@ -17,8 +16,8 @@ import {
 
 //api
 import {
-  getFormBySection,
-  postInstructorForm,
+  GetStudentOutcomesCompleted,
+  SetStudentOutcomesCompleted,
   getSection,
   getGrades,
   setGrades,
@@ -27,35 +26,30 @@ import {
 import GradesInput from "../components/form-components/GradesInput";
 import CourseOutcomesMapping from "../components/form-components/CourseOutcomesMapping";
 import blankForm from "../components/form-components/blankForm.json";
-import outcomeBlankForm from "../components/form-components/outcomeBlankForm.json";
-const formCompletion = ({
-  number,
-  section,
-  semester,
-  year,
-  id,
-  department,
-}) => {
+import Navigation from "../components/instructor-components/Navigation";
+
+const formCompletion = ({ number, section, term, year, id, department }) => {
   const router = useRouter();
-  const [form, setForm] = useState();
   const [gradeForm, setGradeForm] = useState();
   const [outcomeForm, setOutcomeForm] = useState();
-
   const [refreshKey, setRefreshKey] = useState(0); //For refreshing the table
-
-  const [sectionInstructorEUID, setSectionInstructorEUID] = useState();
   const toast = useToast({ position: "top" });
-
   const refreshTable = () => {
     setRefreshKey(refreshKey + 1);
   };
 
   const checkUser = async () => {
     const ISSERVER = typeof window === "undefined";
+    const sectionInstructorEUID = await getSectionInformation();
     let isLoading = true;
     if (!ISSERVER) {
       const token = cookieCutter.get("token");
       const json = jwt.decode(token);
+      if (!json) {
+        router.push("/Login");
+        return;
+      }
+
       const jsonUserId = json.unique_name;
       if (json == null) {
         router.push("/");
@@ -79,7 +73,7 @@ const formCompletion = ({
   const getSectionInformation = async () => {
     try {
       const sectionRes = await getSection(
-        semester,
+        term,
         year,
         department,
         number,
@@ -97,24 +91,21 @@ const formCompletion = ({
         });
         return;
       }
-      setSectionInstructorEUID(sectionData.instructorEUID);
-      console.log(sectionData.instructorEUID);
+      return sectionData.instructorEUID;
     } catch (error) {
       console.log(error);
     }
-    console.log(number, section, semester, year, id, department);
   };
 
   const getGradeForm = async () => {
     try {
-      //const res = await getGrades(year,term,department,number,section)
-      var res = await getGrades("2023", "Spring", "CSCE", "1030", "1");
-      var gradesData = res.data;
+      const res = await getGrades(year, term, department, number, section);
+      const gradesData = res.data;
       if (gradesData) {
         if (Object.keys(gradesData).length < 1) {
           //If the is the first time working on the form then create a blank form
           console.log("No previous form found");
-          await setGrades("2023", "Spring", "CSCE", "1030", "1", blankForm);
+          await setGrades(year, term, department, number, section, blankForm);
           console.log(blankForm);
           setGradeForm(blankForm);
         } else if (Object.keys(gradesData).length >= 1) {
@@ -133,21 +124,20 @@ const formCompletion = ({
     } catch (error) {
       console.log(error);
     }
-    const formData = await getFormBySection(
-      id,
-      2020,
-      "Fall",
-      "CSCE",
-      number.toString(),
-      section
-    );
-    console.log(formData);
-    setForm(formData);
   };
 
   const getOutcomeForm = async () => {
     try {
-      setOutcomeForm(outcomeBlankForm);
+      const outcomeFormRes = await GetStudentOutcomesCompleted(
+        year,
+        term,
+        department,
+        number,
+        section
+      );
+      const outcomeFormData = outcomeFormRes.data;
+      console.log(outcomeFormData);
+      setOutcomeForm(outcomeFormData);
     } catch (error) {
       console.log(error);
     }
@@ -171,7 +161,7 @@ const formCompletion = ({
     var tempForm = outcomeForm;
     for (var i = 0; i < tempForm.length; i++) {
       if (tempForm[i].outcomeName == outcomeName) {
-        tempForm[i][major] = newValue;
+        tempForm[i][major] = String(newValue);
       }
     }
     console.log(tempForm);
@@ -179,36 +169,43 @@ const formCompletion = ({
   };
 
   useEffect(() => {
-    //getSectionInformation();
     getOutcomeForm();
     getGradeForm();
-  }, [refreshKey]);
-
-  useEffect(() => {
-    //getGradeForm();
   }, []);
 
   useEffect(() => {
-    //if (sectionInstructorEUID != null) checkUser();
-  }, [sectionInstructorEUID]);
+    getSectionInformation();
+    checkUser();
+  }, []);
 
   const handleSubmit = async () => {
     console.log(gradeForm);
     console.log("----------------------");
     console.log(outcomeForm);
-    return;
     try {
       //const res = await setGrades(year,term,department,number,section,form2)
-      const submitRes = await setGrades(
-        "2023",
-        "Spring",
-        "CSCE",
-        "1030",
-        "1",
+      const gradeRes = await setGrades(
+        year,
+        term,
+        department,
+        number,
+        section,
         gradeForm
       );
-      const status = submitRes.status;
-      if (status == "Success") {
+
+      const outcomeRes = await SetStudentOutcomesCompleted(
+        year,
+        term,
+        department,
+        number,
+        section,
+        outcomeForm
+      );
+
+      const gradeStatus = gradeRes.status;
+      const outcomeStatus = outcomeRes.status;
+
+      if (gradeStatus == "Success" && outcomeStatus == "Success") {
         toast({
           description: `Form submitted!`,
           status: "success",
@@ -216,9 +213,17 @@ const formCompletion = ({
           isClosable: true,
         });
         return;
-      } else {
+      } else if (!gradeStatus == "Success") {
         toast({
-          description: `There was an error submitting the form! Error:${status}`,
+          description: `There was an error submitting the form! Error:${gradeStatus}`,
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+        return;
+      } else if (!outcomeStatus == "Success") {
+        toast({
+          description: `There was an error submitting the form! Error:${outcomeStatus}`,
           status: "error",
           duration: 3000,
           isClosable: true,
@@ -228,64 +233,69 @@ const formCompletion = ({
     } catch (error) {
       console.log(error);
     }
+
     refreshTable();
   };
 
   return (
-    <Center>
-      {form ? (
-        <Flex mt="2em" direction="column" w="90%">
-          <Box w="80%">
-            <Text fontSize="2xl" fontWeight="bold">
-              {department} {number}.{section}
-            </Text>
-            <Text fontSize="xl" fontWeight="bold" color="green" mb="2em">
-              ABET Course Assesment
-            </Text>
-          </Box>
-          <GradesInput
-            csGrades={gradeForm.CS}
-            ceGrades={gradeForm.CE}
-            itGrades={gradeForm.IT}
-            cysGrades={gradeForm.CYS}
-            handleGradeChange={handleGradeChange}
-          />
+    <div>
+      <Navigation />
+      <Center>
+        {gradeForm && outcomeForm ? (
+          <Flex mt="2em" direction="column" w="90%">
+            <Box w="80%">
+              <Text fontSize="2xl" fontWeight="bold">
+                {department} {number}.{section}
+              </Text>
+              <Text fontSize="xl" fontWeight="bold" color="green" mb="2em">
+                ABET Course Assesment
+              </Text>
+            </Box>
+            <GradesInput
+              csGrades={gradeForm.CS}
+              ceGrades={gradeForm.CE}
+              itGrades={gradeForm.IT}
+              cysGrades={gradeForm.CYS}
+              handleGradeChange={handleGradeChange}
+            />
 
-          <CourseOutcomesMapping
-            courseOutcomes={outcomeForm}
-            handleOutcomesChange={handleOutcomesChange}
-          />
-          <Text fontSize="xl" fontWeight="bold" mb="1em">
-            Instructor Comments
-          </Text>
-          <Textarea
-            mb="1em"
-            size="lg"
-            bg="#edf2f7"
-            placeholder="// Write a comment"
-          ></Textarea>
-          <Box>
-            <Button
+            <CourseOutcomesMapping
+              courseOutcomes={outcomeForm}
+              handleOutcomesChange={handleOutcomesChange}
+            />
+            <Text fontSize="xl" fontWeight="bold" mb="1em">
+              Instructor Comments
+            </Text>
+            <Textarea
               mb="1em"
-              colorScheme="green"
-              w="max-content"
-              onClick={handleSubmit}
-            >
-              Submit Report
-            </Button>
-          </Box>
-        </Flex>
-      ) : null}
-    </Center>
+              size="lg"
+              bg="#edf2f7"
+              placeholder="// Write a comment"
+            ></Textarea>
+            <Box>
+              <Button
+                mb="1em"
+                colorScheme="green"
+                w="max-content"
+                onClick={handleSubmit}
+              >
+                Submit Report
+              </Button>
+            </Box>
+          </Flex>
+        ) : null}
+      </Center>
+    </div>
   );
 };
 
 formCompletion.getInitialProps = ({ query }) => {
+  console.log(query);
   return {
     department: query.department,
     number: query.number,
     section: query.section,
-    semester: query.semester,
+    term: query.term,
     year: query.year,
     id: "MT2020",
   };
